b2616c36c3e14e313e295de3a0d3c00551ef2f8b
spongefile <aspiala@gmail.com>
2023-10-21 12:58:12 +0300

    Added event listeners for pause and resume buttons

--- assets/index.js
+++ assets/index.js
@@ -7,3 +7,7 @@ const t = new Timekeeper().attach(document.querySelector('form'));
 q(document, 'a[href$="#alarm"]', el => on(el, 'click', e => t.toggleAlarm(e)));
 q(document, 'a[href^="#start:"]', el => on(el, 'click', () => t.start(el.href.split('#start:')[1])));
 q(document, 'a[href^="#stop"]', el => on(el, 'click', () => t.stop()));
+
+//Event listeners for Pause and Resume buttons
+q(document, '#pauseButton', el => on(el, 'click', () => t.pause()));
+q(document, '#resumeButton', el => on(el, 'click', () => t.resume()));
